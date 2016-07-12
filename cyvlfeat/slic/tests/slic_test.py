32848032202152a7851537402eebf8d323d0738c
Simmi Mourya <simmimourya@outlook.com>
2016-07-12 19:14:15 +0530

    Correct Syntax error

--- cyvlfeat/slic/tests/slic_test.py
+++ cyvlfeat/slic/tests/slic_test.py
@@ -3,9 +3,9 @@ import numpy as np
 from numpy.testing import assert_allclose
 from cyvlfeat.test_util import lena
 
-
 img = lena().astype(np.float32)
 
+
 def test_slic_segment():
-    segment = slic(img, region_size = 10, regularizer = 10):
+    segment = slic(img, region_size=10, regularizer=10)
     assert segment.shape[0] == 124241
