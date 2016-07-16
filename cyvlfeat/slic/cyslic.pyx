0fd80198e303b2a6b5462e96500d0fdce2dd48c6
Simmi Mourya <simmimourya@outlook.com>
2016-07-16 15:53:56 +0530

    correct datatype for segmentation and few other modifications

--- cyvlfeat/slic/cyslic.pyx
+++ cyvlfeat/slic/cyslic.pyx
@@ -4,6 +4,7 @@
 # This file is part of the VLFeat library and is made available under
 # the terms of the BSD license (see the COPYING file).
 
+# no need for division import -> we need already integer output here -> 2
 import numpy as np
 cimport numpy as np
 import ctypes
@@ -25,30 +26,26 @@ cpdef cy_slic(np.ndarray[float, ndim=2, mode='c'] image, int region_size,
         vl_size n_channels = 1
         # vl_size n_channels = image.shape[2]
         int min_region_size = -1
+        np.ndarray[unsigned int, ndim=2, mode='c'] segmentation
 
     if min_region_size < 0:
         #FIXME: Check weather C returns a float here by division.
         # (region_size * region_size) / (6*6)
         min_region_size = region_size * region_size/36
-        print(min_region_size)
         printf("min_reigon_size cannot be less than 0. Assigning min_reigon_size = %d\n", min_region_size)
 
     if verbose:
-            printf("vl_slic: image:         [%d x %d x %d]\n",
-                   width, height, n_channels)
-            printf("vl_slic: region size:           %d\n",
-                   region_size)
-            printf("vl_slic: regularizer:           %d\n",
-                   regularizer)
-            printf("vl_slic: min region size:           %d\n",
-                   min_region_size)
+         printf(regularizer)
+            # printf("vl_slic: image:         [%d x %d x %d]\n",
+            #        width, height, n_channels)
+            # printf("vl_slic: region size:           %d\n",
+            #        region_size)
+            # # print("vl_slic: regularizer:    "regularizer)
+            # printf("vl_slic: regularizer:           %d\n", regularizer)
+            # printf("vl_slic: min region size:           %d\n",
+            #        min_region_size)
 
-    # cdef segmentation = np.zeros(height, width, dtype=np.float32)
-
-    # cdef np.ndarray[int, ndim=2, mode='c'] segmentation = np.zeros((height, width), dtype=ctypes.c_int)
-    cdef np.ndarray[float, ndim=2, mode='c'] segmentation = np.zeros((height, width), dtype=ctypes.c_float)
-
-    # cdef float[:] segmentation = np.zeros(height, width, dtype=np.float32)
+    segmentation = np.zeros((height, width), dtype=np.uint32, order='C')
 
     vl_slic_segment(&segmentation[0,0], &image[0, 0], height, width, n_channels,
                     region_size, regularizer, min_region_size)
